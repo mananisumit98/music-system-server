8596dd0522a455a5a1987e956a2d0292c3c95555
sumit.m.manani <=>
2024-07-11 22:37:49 +0530

    Removed exxess code from index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
-const Users = require("./models/usersModal");
 // Import Routes
 const userRoutes = require("./routes/users");
 const playlistRoutes = require("./routes/playlists");
@@ -11,7 +10,6 @@ const songRoutes = require("./routes/songs");
 
 const PORT = 8000;
 const CONNECTION_URL = "mongodb+srv://music:system2024@musicsystem.b4ps5zm.mongodb.net/music-system";
-// const CONNECTION_URL = "mongodb://localhost:27017/music-system";
 
 app.use(cors({
     origin: ["http://localhost:3000", "https://music-playlist-client.vercel.app"]
@@ -24,19 +22,7 @@ app.use("/playlist", playlistRoutes);
 app.use("/songs", songRoutes);
 
 app.get("/", (req, res) => {
-    return res.send({ message: "Server for Music System is Started : Version - 1" });
-});
-app.get("/vercel", (req, res) => {
-    return res.send({ message: "Express running on vercel : Version -  1" });
-});
-app.get("/test", async (req, res) => {
-    try {
-        const data = await Users.find({});
-        console.log("data :::: ", data);
-        return res.send({ message: "Test Data", data: data });
-    } catch (error) {
-        return res.send({ message: "Error", data: error.message });
-    }
+    return res.send({ message: "Express Server for Music System is running on vercel" });
 });
 
 mongoose.connect(CONNECTION_URL)
@@ -49,10 +35,3 @@ mongoose.connect(CONNECTION_URL)
     .catch((error) => {
         console.error("Database connection error: ", error);
     });
-
-// app.listen(PORT, () => {
-//     console.log("Server is running on port 8000");
-//     mongoose.connect(CONNECTION_URL).then(() => {
-//         console.log("Database is connected successfully");
-//     });
-// });
